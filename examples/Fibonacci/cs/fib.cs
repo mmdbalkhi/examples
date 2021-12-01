9b9d643e15bc26911849dcc6df4a18a894957662
Komeil Parseh <komeilparseh@disroot.org>
2021-12-01 18:00:57 +0330

    add cs code

---
+++ examples/Fibonacci/cs/fib.cs
@@ -0,0 +1,21 @@
+﻿public class game
+{
+
+    static int fib(int number)
+    {
+        switch (number)
+        {
+            case 1:
+                return 1;
+            case 2:
+                return 1;
+            default:
+                return fib(number - 1) + fib(number - 2);
+        }
+    }
+    static void Main()
+    {
+        Console.WriteLine(fib(10));
+    }
+
+}
